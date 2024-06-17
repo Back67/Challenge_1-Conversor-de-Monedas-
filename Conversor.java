4ed74c47130dbec2e9f566a1fa6eb7e4d304ae30
Back67 <168211751+Back67@users.noreply.github.com>
2024-06-16 19:45:11 -0600

    Create Conversor.java

---
+++ Conversor.java
@@ -0,0 +1,12 @@
+package com.alura.conversor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConversorDeMonedasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConversorDeMonedasApplication.class, args);
+    }
+}
